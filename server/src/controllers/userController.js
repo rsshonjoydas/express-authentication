1f18363d32de89fb130f6cd83d18e7ebd7771566
rsshonjoydas <rsshonjoydas@gmail.com>
2022-03-11 19:15:24 +0600

    verify email

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,6 +1,12 @@
 import bcrypt from "bcryptjs";
-import User from "../models/data-models/user-data-model";
+import jwt from "jsonwebtoken";
+import env from "../config";
+import DataModels from "../models/data-models";
 import { checkUser } from "../services/userServices";
+import JWTToken from "../utils/JWTToken";
+import sendMail from "./sendMail";
+
+const DataModel = DataModels.User;
 
 /**
  * @param  {} req
@@ -23,17 +29,47 @@ export const register = async (req, res) => {
     const hashPassword = await bcrypt.hash(password, salt);
 
     // ? save user to database
-
-    await User.create({
+    const newUser = {
       firstName,
       lastName,
       username,
       password: hashPassword,
-    });
+    };
 
-    res.json({ message: "Authentication Successful!" });
+    const activationToken = JWTToken.activationToken(newUser);
+
+    const url = `${env.APP_URL}/auth/verify/${activationToken}`;
+    sendMail(username, url, "Verify your email address");
+
+    res.json({ message: "Verify your email address!" });
   } catch (error) {
     res.status(400).json({ message: "Authentication Failed!" });
+  }
+  return true;
+};
+
+/**
+ * @param  {} req
+ * @param  {} res
+ * @desc Email verify
+ * @route POST /auth/verify
+ * @access private
+ * @function {@link activateEmail}
+ */
+export const activateEmail = async (req, res) => {
+  try {
+    const { activationToken } = req.body;
+    const user = jwt.verify(activationToken, env.JWT_ACTIVATION_TOKEN);
+
+    const { firstName, lastName, username, password } = user;
+
+    const newUser = new DataModel({ firstName, lastName, username, password });
+
+    await newUser.save();
+
+    res.json({ message: "Email Verified!" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
   }
   return true;
 };
--- server/src/routes/userRoute.js
+++ server/src/routes/userRoute.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { register } from "../controllers/userController";
+import { activateEmail, register } from "../controllers/userController";
 import { handleValidation } from "../middlewares";
 import validators from "../models/request-models";
 
 const router = express.Router();
 
-router.post("/register", handleValidation(validators.userSchemaValidate), register);
+router
+  .post("/register", handleValidation(validators.userSchemaValidate), register)
+  .post("/verify", activateEmail);
 
 export default router;
